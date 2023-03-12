fb0a66d26a27edcf5f3dc995dc58e6b14ce4ac04
Kent O'Sullivan <34455912+12Kentos@users.noreply.github.com>
2023-03-11 19:46:38 -0700

    Home page Mobile Semi-Done

---
+++ src/pages/Homepage.js
@@ -0,0 +1,24 @@
+import styles from "./Homepage.module.scss";
+
+const HomePage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles["inner-wrapper"]}>
+        <h1>
+          So, you want to travel to <div className={styles.space}>Space</div>
+        </h1>
+        <p>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+        <div className={styles["explore-wrapper"]}>
+          <p className={styles.explore}>Explore</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
